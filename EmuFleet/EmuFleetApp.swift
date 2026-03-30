6c203d33231ee0b6093ef23b0451d9a31f92a53d
Ngonidzashe Mangudya <imngonii@gmail.com>
2026-03-30 16:42:07 +0300

    Initial Commit

---
+++ EmuFleet/EmuFleetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmuFleetApp.swift
+//  EmuFleet
+//
+//  Created by Ngonidzashe  Mangudya on 2026/03/30.
+//
+
+import SwiftUI
+
+@main
+struct EmuFleetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
